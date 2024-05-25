cfd4422747251eaa5dc027ec87fdd9789094ee4e
linhvuquach <quachvulinhag1997@gmail.com>
2024-05-25 16:58:05 +0700

    [DP-32] feat: C# integral numeric types

---
+++ csharp/types/value-types/integral-numberic-types/Integral-Numberic-Types/Program.cs
@@ -0,0 +1,23 @@
+﻿// Characteristics of the integral types
+
+sbyte a = 5;
+byte b = 5;
+short c = 5;
+ushort d = 5;
+int e = 5;
+uint f = 5;
+long g = 5;
+ulong h = 5;
+nint i = 5;
+nuint j = 5;
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(d);
+Console.WriteLine(e);
+Console.WriteLine(f);
+Console.WriteLine(g);
+Console.WriteLine(h);
+Console.WriteLine(i);
+Console.WriteLine(j);
---
+++ csharp/types/value-types/integral-numberic-types/Integral-Numberic-Types/README.md
@@ -0,0 +1,3 @@
+# Integral numeric types
+
+The integral numeric types represent integer numbers. All integral numeric types are `value types`.
