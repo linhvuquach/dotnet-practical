92a7b7f2997540f67127ba7528bbfb5350844227
linh.vu.quach <quachvulinhag1997@gmail.com>
2024-06-10 18:32:46 +0700

    feat: async examples

---
+++ csharp/asynchronous-programming/examples/Execute.Multiple.Tasks/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Task task1 = Task.Run(() => DoWork(1));
+Task task2 = Task.Run(() => DoWork(2));
+Task task3 = Task.Run(() => DoWork(3));
+
+// Await thier completion
+await Task.WhenAll(task1, task2, task3); // The `Task.WhenAll` method can be used to wait for multiple tasks to complete.
+
+Console.WriteLine("All tasks completed.");
+
+void DoWork(int taskNumber)
+{
+    // Simulate work
+    Console.WriteLine($"Task {taskNumber} starting.");
+    Task.Delay(2000).Wait(); // Synchronous wait to simulate work
+    Console.WriteLine($"Task {taskNumber} completed.");
+}
---
+++ csharp/asynchronous-programming/examples/Task.Run/Program.cs
@@ -0,0 +1,17 @@
+﻿async Task PerformTaskAsync()
+{
+    Console.WriteLine("Start of async method");
+
+    // The `Task.Run` method is used to run CPU-bound tasks on a background thread
+    // Using Task.Run to run a synchronous method asynchronously
+    await Task.Run(() => PerformSynchronousTask());
+
+    Console.WriteLine("End of async method");
+}
+
+static async void PerformSynchronousTask()
+{
+    Console.WriteLine("Executing synchronous task.");
+}
+
+await PerformTaskAsync();
