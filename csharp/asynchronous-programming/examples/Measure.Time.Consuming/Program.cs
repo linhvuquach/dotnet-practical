10292fcbc1044b44570cfdac93dbe822d0c70728
linh.vu.quach <quachvulinhag1997@gmail.com>
2024-06-11 15:07:27 +0700

    [dp-10] feat: measure sycn and async time consuming

---
+++ csharp/asynchronous-programming/examples/Measure.Time.Consuming/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics;
+
+// Synchronous
+TimeSpan syncTimeTaken = MeasureTime(() =>
+{
+    SyncUtil.SimulateLongRunningTask();
+    SyncUtil.SimulateLongRunningTask();
+    SyncUtil.SimulateLongRunningTask();
+});
+Console.WriteLine($"Sync time taken: {syncTimeTaken.TotalSeconds} seconds");
+
+// Asynchronous
+TimeSpan asyncTimeTaken = MeasureTime(async () =>
+{
+    await getAsync(1);
+    await getAsync(2);
+    await getAsync(3);
+});
+Console.WriteLine($"Async time taken: {asyncTimeTaken.TotalSeconds} seconds");
+
+// Edge case: Async method waiting together
+TimeSpan edgeCaseTimeTaken = MeasureTime(async () =>
+{
+    var result1 = await getAsync(1);
+    var result2 = await getAsync(result1);
+    var result3 = await getAsync(result2);
+});
+
+Console.WriteLine($"EdgeCase time taken: {edgeCaseTimeTaken.TotalSeconds} seconds");
+
+
+static async Task<int> getAsync(int input)
+{
+    await Task.Delay(2000); // waits for 2 seconds
+    return input;
+}
+
+/* Util functions */
+static TimeSpan MeasureTime(Action action)
+{
+    Stopwatch stopwatch = Stopwatch.StartNew();
+    stopwatch.Start();
+    action();
+    stopwatch.Stop();
+
+    return stopwatch.Elapsed;
+}
+
+static class SyncUtil
+{
+    public static void SimulateLongRunningTask()
+    {
+        Thread.Sleep(2000); // waits for 2 seconds
+    }
+}
